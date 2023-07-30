3e332da6f5efa8f64f56eb82d61adeba288ce8ee
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-07-30 12:20:27 +0700

    feat: Implement Firebase registration logic

---
+++ app/src/main/java/com/example/authfirebase/Register.java
@@ -0,0 +1,56 @@
+package com.example.authfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register extends AppCompatActivity {
+
+    private EditText etUsername, etEmail, etPassword;
+    private Button btnRegister;
+
+    private DatabaseReference database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUsername);
+        etEmail= findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://authfirebase-4d262-default-rtdb.firebaseio.com/");
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if(username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Register.this, "Ada data yang masih kosong", Toast.LENGTH_SHORT).show();
+                }else {
+                    database = FirebaseDatabase.getInstance().getReference("users");
+                    database.child(username).child("username").setValue(username);
+                    database.child(username).child("email").setValue(email);
+                    database.child(username).child("password").setValue(password);
+
+                    Toast.makeText(getApplicationContext(), "Register Berhasil", Toast.LENGTH_SHORT).show();
+                    Intent register = new Intent(getApplicationContext(), Login.class);
+                    startActivity(register);
+                }
+            }
+        });
+
+    }
+}
